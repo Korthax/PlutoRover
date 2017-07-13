d1bbfc7b4128ff905ba83c8bcc2a4d6187ad429e
Korthax <stephenneilphillips@gmail.com>
2017-07-13 20:02:36 +0100

    Added dummy rover api and an initial api test around it.

---
+++ PlutoRover.Core/RoverApi.cs
@@ -0,0 +1,10 @@
+﻿namespace PlutoRover.Core
+{
+    public class RoverApi
+    {
+        public string Process(string input)
+        {
+            return "0,0,N";
+        }
+    }
+}
---
+++ tests/PlutoRover.Tests/ApiTests/GivenARoverApi/WithNoInput/WhenProcessing.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using PlutoRover.Core;
+using Xunit;
+
+namespace PlutoRover.Tests.ApiTests.GivenARoverApi.WithNoInput
+{
+    public class WhenProcessing
+    {
+        [Fact]
+        public void ThenTheStartingLocationIsReturned()
+        {
+            var subject = new RoverApi();
+            var result = subject.Process(null);
+
+            result.Should().Be("0,0,N");
+        }
+    }
+}
