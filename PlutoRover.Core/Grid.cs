b08f52f6d43e5c8a964146f36b32670d86c285ed
Korthax <stephenneilphillips@gmail.com>
2017-07-13 21:44:55 +0100

    Added a grid class

---
+++ PlutoRover.Core/Grid.cs
@@ -0,0 +1,31 @@
+﻿namespace PlutoRover.Core
+{
+    public class Grid
+    {
+        private readonly bool[,] _grid;
+
+        public static Grid InitialiseFrom(int width, int height)
+        {
+            var data = new bool[width, height];
+            return new Grid(data);
+        }
+
+        private Grid(bool[,] grid)
+        {
+            _grid = grid;
+        }
+
+        public Position Wrap(Position position)
+        {
+            var x = position.X < _grid.GetUpperBound(0)
+                ? position.X < 0 ? _grid.GetUpperBound(0) : position.X
+                : 0;
+
+            var y = position.Y < _grid.GetUpperBound(1)
+                ? position.Y < 0 ? _grid.GetUpperBound(1) : position.Y
+                : 0;
+
+            return new Position(x, y);
+        }
+    }
+}
---
+++ tests/PlutoRover.Tests/UnitTests/GivenAGrid/WithAPositionOffTheGrid/WhenWrapping.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using PlutoRover.Core;
+using Xunit;
+
+namespace PlutoRover.Tests.UnitTests.GivenAGrid.WithAPositionOffTheGrid
+{
+    public class WhenWrappingAPosition
+    {
+        [Theory]
+        [InlineData(-1, 10, 99, 10)]
+        [InlineData(100, 10, 0, 10)]
+        [InlineData(10, -1, 10, 99)]
+        [InlineData(10, 100, 10, 0)]
+        public void ThenThePositionIsWrappedAroundToTheOtherSideCorrectly(int x, int y, int expectedX, int expectedY)
+        {
+            var subject = Grid.InitialiseFrom(100, 100);
+            var result = subject.Wrap(new Position(x, y));
+
+            result.Should().Be(new Position(expectedX, expectedY));
+        }
+    }
+}
